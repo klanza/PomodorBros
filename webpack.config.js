b5fd386d2d1736bdb6b09a40da0e7daa11567ac2
TSchaeken-Deskop <TSchaeken@gmail.com>
2018-08-29 22:17:09 -0700

    starter setup with webpack4/babel6

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html'
+});
+
+module.exports = {
+  entry: path.resolve('./src/index.js'),
+  output: {
+    path: path.resolve('build'),
+    filename: 'index_bundle.js'
+  },
+  devServer: {
+    port: 8000,
+    historyApiFallback: true,
+    inline: true
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    modules: ['src', 'node_modules']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
