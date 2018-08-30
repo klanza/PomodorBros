eff40c9cef5b8ff2597adb310d57aeaf0db54c67
klanza <klanza91@gmail.com>
2018-08-29 23:21:19 -0700

    Building timer component

--- src/Components/Timer/Timer.js
+++ src/Components/Timer/Timer.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 
-class Timer extends component {
+class Timer extends Component {
+  constructor() {
+    super();
+    this.state = { number: 10 };
+  }
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState({
+        number: this.state.number--
+      });
+      console.log(number);
+    }, 1000);
+  }
+
   render() {
-    return <div>butts</div>;
+    return <div>{this.state.number}</div>;
   }
 }
 
